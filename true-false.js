179bfe64aadd3a2dbd1a2cdc7380c56e67c94db0
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-08-13 14:03:27 +0600

    feat[added]true-false.js

---
+++ true-false.js
@@ -0,0 +1,11 @@
+// Falsy Values List: 0, false, "", null, NaN, undefined
+
+// truthy values list: '0', ' ', [], {}
+
+// to check assign any value into a and see the output.
+const a = 0;
+if (a) {
+  console.log("a is truthy");
+} else {
+  console.log("a is falsy");
+}
