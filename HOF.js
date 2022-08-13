29b902a25b3c40d40a1db4c3b898f4deb678c23d
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-08-13 19:24:20 +0600

    feat[added HOF.js]

---
+++ HOF.js
@@ -0,0 +1,40 @@
+// In JavaScript, functions are treated as first-class objects. That means they can be stored as any other values in objects or arrays, passed around as arguments, or returned from other functions.
+
+// Higher-order functions are a functional programming pattern when functions are being passed as arguments to other functions or returned as a result.
+
+function log(item) {
+  return console.log(item); // 1, 2, 3
+}
+
+function process(data, callback) {
+  for (let i = 0; i < data.length; i++) {
+    callback(data[i]);
+  }
+}
+
+process([1, 2, 3], log);
+
+// Array HoF
+
+const numbers = [1, 2, 3];
+
+// pass unonymouse function
+numbers.map(function (item) {
+  return item * 2;
+}); // [2, 4, 6]
+
+// or extract the callback into a named function
+function double(item) {
+  return item * 2;
+}
+
+numbers.map(double); // [2, 4, 6]
+
+const numbers2 = [1, 2, 3];
+
+console.log(
+  numbers
+    .map((n) => n * 2) // it will return [2, 4, 6]
+    .filter((n) => n % 4) // it will filter out number that divides by 4
+    .reduce((a, b) => a + b) // return 6 - sum of the array items
+);
