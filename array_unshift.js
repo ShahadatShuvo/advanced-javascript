e2038bb5a2c6eb0dc5abab0af2bb382f7cd05db8
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-08-27 07:09:56 +0600

    array unshift

---
+++ array_unshift.js
@@ -0,0 +1,8 @@
+let arr = ["banana", "orange", "apple"]; // + 'maango'
+console.log(arr);
+arr.push("mango");
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.unshift("mango");
+console.log(arr);
