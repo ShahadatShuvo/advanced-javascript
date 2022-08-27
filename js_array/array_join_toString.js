d370c7a021da0ca85c235cf4e9be433d4ee44092
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-08-27 08:23:30 +0600

    new file added

---
+++ js_array/array_join_toString.js
@@ -0,0 +1,13 @@
+let arr = ["rahim", "karim", "mahdi", "shuvo", "shahadat"];
+
+let str = arr.toString();
+
+console.log(str); // rahim,karim,mahdi,shuvo,shahadat
+
+let str1 = arr.join(" - ");
+
+console.log(str1); // rahim - karim - mahdi - shuvo - shahadat
+
+let str2 = arr.join(" ");
+
+console.log(str2); // rahim karim mahdi shuvo shahadat
