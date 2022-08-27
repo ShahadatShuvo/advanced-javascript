41dea92515d1576a8e2343f463f1f8a8dae2e075
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-08-27 07:16:01 +0600

    ja array shift unshift

--- js_array/array_shift_unshift.js
+++ js_array/array_shift_unshift.js
@@ -4,5 +4,7 @@ arr.push("mango");
 console.log(arr);
 arr.pop();
 console.log(arr);
-arr.unshift("mango");
+arr.unshift("mango"); // adds element from first index
+console.log(arr);
+arr.shift(); // removes element fron first
 console.log(arr);
