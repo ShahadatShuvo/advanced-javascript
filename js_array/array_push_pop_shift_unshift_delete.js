0820deb35e6936832b3bfc4223ab553fe7d074a0
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-08-27 07:33:44 +0600

    updated

--- js_array/array_push_pop_shift_unshift_delete.js
+++ js_array/array_push_pop_shift_unshift_delete.js
@@ -8,3 +8,9 @@ arr.unshift("mango"); // adds element from first index
 console.log(arr);
 arr.shift(); // removes element fron first
 console.log(arr);
+
+delete arr[1];
+console.log(arr);
+
+arr[1] = "guava";
+console.log(arr);
