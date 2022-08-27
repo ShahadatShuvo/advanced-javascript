fbc58c868cade5e7e4b1d1d335643054201f1367
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-08-27 07:38:10 +0600

    reUpdate

--- js_array/array_push_pop_shift_unshift_delete.js
+++ js_array/array_push_pop_shift_unshift_delete.js
@@ -1,16 +1,16 @@
 let arr = ["banana", "orange", "apple"]; // + 'maango'
-console.log(arr);
+console.log(arr); // [ 'banana', 'orange', 'apple' ]
 arr.push("mango");
-console.log(arr);
+console.log(arr); // [ 'banana', 'orange', 'apple', 'mango' ]
 arr.pop();
-console.log(arr);
+console.log(arr); // [ 'banana', 'orange', 'apple' ]
 arr.unshift("mango"); // adds element from first index
-console.log(arr);
+console.log(arr); // [ 'mango', 'banana', 'orange', 'apple' ]
 arr.shift(); // removes element fron first
-console.log(arr);
+console.log(arr); // [ 'banana', 'orange', 'apple' ]
 
 delete arr[1];
-console.log(arr);
+console.log(arr); // [ 'banana', , 'apple' ]
 
 arr[1] = "guava";
-console.log(arr);
+console.log(arr); // [ 'banana', 'guava', 'apple' ]
