58422471740110ba49fb4e62e185785173fe1fab
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-08-30 13:03:38 +0600

    updated code

--- OOP/custom_error_class.js
+++ OOP/custom_error_class.js
@@ -1,6 +1,6 @@
 class NotFoundError extends Error {
   constructor(name, message) {
-    super(message);
+    super(message); // we can giva as rest  ...rest
     this.name = "NotFoundError";
   }
   toJSON() {
@@ -15,6 +15,11 @@ try {
 } catch (error) {
   if (error instanceof NotFoundError) {
     console.log("yes"); // yes
+
+    console.log({
+      error: 404,
+      details: error.toJSON(),
+    }); // { error: 404, details: { name: 'NotFoundError', message: 'This user is not Found' } }
   }
   console.log(error); // { [NotFoundError: This user is not Found] name: 'NotFoundError' }
   console.log(error.message); // This user is not Found
