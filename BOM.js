996d6e8f27baab732048810a8765c08f0bfc5eb7
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-09-02 20:21:36 +0600

    updated

---
+++ BOM.js
@@ -0,0 +1,13 @@
+// BOM: Browser Object Model
+
+// Browser Object Model (BOM) is a global object that contains references to the browser window and its document.
+
+// Browser r window te ja kiso ase shob BOM r under e; eta ekta Object
+
+// The window object is supported by all browsers. It represents the browser's window.
+
+// document hocche window object r memebr
+
+// document.getElementById( )
+
+console.log(window.innerHeight); // 695
